316ccee596930f32fe7cab02778586cace954067
Simon Smith <simonsmith5521@gmail.com>
2021-11-23 19:03:19 +0000

    Update node.lua

--- node.lua
+++ node.lua
@@ -2,6 +2,8 @@ gl.setup(NATIVE_WIDTH, NATIVE_HEIGHT)
 
 util.no_globals()
 
+local st
+
 local function image(file, duration)
     local img, ends
     return {
@@ -152,6 +154,7 @@ local playlist_2 = Scheduler()
 
 util.json_watch("config.json", function(config)
     print "my config.json changed"
+    st = util.screen_transform(config.rotation)
     playlist_1.update(config.playlist_1)
     playlist_2.update(config.playlist_2)
 end)
@@ -171,8 +174,9 @@ local runner_2 = Runner(playlist_2, {
 })
 
 function node.render()
-    -- gr.clear(0,0,0,1)
+    gr.clear(0,0,0,1)
     -- local st = util.screen_transform(config.rotation)
+    print "node.render"
     -- st()
     runner_1.tick()
     runner_2.tick()
