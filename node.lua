fbe9c27e99ae7cedd817565c7cda90d45ae04899
Simon Smith <simonsmith5521@gmail.com>
2021-11-23 19:09:40 +0000

    Update node.lua

--- node.lua
+++ node.lua
@@ -174,7 +174,7 @@ local runner_2 = Runner(playlist_2, {
 })
 
 function node.render()
-    gl.clear(0,0,0,1)
+    -- gl.clear(0,0,0,1)
     -- local st = util.screen_transform(config.rotation)
     print "node.render"
     -- st()
